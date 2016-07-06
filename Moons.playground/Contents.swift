6c6c19c5299170f74bef1a6f1ba18388486663bb
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-06 13:41:42 -0400

    Planet funcs donezo

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -1,23 +1,15 @@
-func moonsOfJupiter() {
-    print("There are 67 moons orbiting Jupiter.")
+func moonsOfJupiter () {
+    print ("There are 67 moons orbiting Jupiter.")
 }
+
 
 moonsOfJupiter()
 
-
-// planetDescription, take 1
-// The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
-/*
-func planetDescription(planet: String) {
-    print("There are 67 moons orbiting \(planet).")
+func planetDescription (planet: String) {
+    print ("There are 67 moons orbiting \(planet).")
 }
 
 planetDescription("Jupiter")
-
-var planet = "Mars"
-
-planetDescription(planet)
-*/
 
 
 func greet(name: String, greeting: String) {
@@ -29,9 +21,6 @@ func planetDescription(planet: String, numberOfMoons: Int) {
     print("There are \(numberOfMoons) moons orbiting \(planet)")
 }
 
-// This line of code gives an error.
-// Uncomment it to see what the error is!
-//planetDescription("Jupiter", 67)
 
 planetDescription("Jupiter", numberOfMoons: 67)
 
@@ -41,3 +30,23 @@ planetDescription(planet, numberOfMoons: moons)
 planet = "Mars"
 moons = 2
 planetDescription(planet, numberOfMoons: moons)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
